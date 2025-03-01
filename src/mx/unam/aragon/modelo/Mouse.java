4aa005b4665e8e7b2358741647ceceb4ead2996b
samzhand <edgar18tor@outlook.com>
2025-02-28 21:34:46 -0600

    Agregue Atributos, Setters y Getters

--- src/mx/unam/aragon/modelo/Mouse.java
+++ src/mx/unam/aragon/modelo/Mouse.java
@@ -2,5 +2,56 @@ package mx.unam.aragon.modelo;
 
 public class Mouse {
     private String color;
-    private String h;
+    private String marca;
+    private String tipo;
+    private int dpi;
+    private boolean inalambrico;
+
+    public Mouse (String color, String marca, String tipo,int dpi,boolean inalambrico){
+        this.color=color;
+        this.marca=marca;
+        this.dpi=dpi;
+        this.tipo=tipo;
+        this.inalambrico=inalambrico;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(int dpi) {
+        this.dpi = dpi;
+    }
+
+    public boolean isInalambrico() {
+        return inalambrico;
+    }
+
+    public void setInalambrico(boolean inalambrico) {
+        this.inalambrico = inalambrico;
+    }
 }
