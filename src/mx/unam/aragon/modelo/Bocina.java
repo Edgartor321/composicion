d526b3e06a62ff27f0df7c6e5b5b5f97222b3985
samzhand <edgar18tor@outlook.com>
2025-03-01 20:31:49 -0600

    Agregue Atributos, Setters y Getters

--- src/mx/unam/aragon/modelo/Bocina.java
+++ src/mx/unam/aragon/modelo/Bocina.java
@@ -1,4 +1,57 @@
 package mx.unam.aragon.modelo;
 
 public class Bocina {
+    private double decibeles;
+    private String marca;
+    private int diametro;
+    private double impedancia;
+    private double voltaje;
+
+    public Bocina(double decibeles,String marca,int diametro,double impedancia,double voltaje){
+        this.decibeles=decibeles;
+        this.marca=marca;
+        this.diametro=diametro;
+        this.impedancia=impedancia;
+        this.voltaje=voltaje;
+    }
+
+    public double getDecibeles() {
+        return decibeles;
+    }
+
+    public void setDecibeles(double decibeles) {
+        this.decibeles = decibeles;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getDiametro() {
+        return diametro;
+    }
+
+    public void setDiametro(int diametro) {
+        this.diametro = diametro;
+    }
+
+    public double getImpedancia() {
+        return impedancia;
+    }
+
+    public void setImpedancia(double impedancia) {
+        this.impedancia = impedancia;
+    }
+
+    public double getVoltaje() {
+        return voltaje;
+    }
+
+    public void setVoltaje(double voltaje) {
+        this.voltaje = voltaje;
+    }
 }
