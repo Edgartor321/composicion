2216fdf7ccc91452884b6c047f1f9baa0ee1898c
samzhand <edgar18tor@outlook.com>
2025-03-18 08:51:19 -0600

    Ya quedo

--- src/mx/unam/aragon/inicio/Inicio.java
+++ src/mx/unam/aragon/inicio/Inicio.java
@@ -3,23 +3,25 @@ import mx.unam.aragon.modelo.*;
 
 public class Inicio {
     public static void main(String[] args) {
-        Teclado teclado =new Teclado();
+        Teclado teclado =new Teclado("QWERTY",68,"Negro","Sin marca",true);
         Mouse raton = new Mouse("rojo","logitech","gamer",10000,false);
         Bocina parlante=new Bocina(1.58,"Bosse",15,370.8,12.5);
+        Procesador cpu =new Procesador("Intel","I9-12900K",8,3200,false);
         Computadora pc= new Computadora();
         Monitor monitor = new Monitor("verde","Sony","OLED",80,false);
         Monitor monitorDos =new Monitor("gris","LG","LED",12,true);
-        Monitor monitorTres =new Monitor("negro","Samsung","QLED",34,false);
 
         pc.setMouse(raton);
         System.out.println(pc.getMouse());
         pc.setBocina(parlante);
         System.out.println(pc.getBocina());
-        //pc.setMonitor(monitor)[0]=monitor;
         System.out.println(pc.getMonitor()[0]=monitor);
         pc.getMonitor()[1]=monitorDos;
         System.out.println(pc.getMonitor()[1]);
-        System.out.println(pc.getMonitor()[2]=monitorTres);
+        pc.setTeclado(teclado);
+        pc.setProcesador(cpu);
+        System.out.println(pc.getTeclado());
+        System.out.println(pc.getProcesador());
 
 
 
